9a2010cb42a5df5c78eff6db82c102436ff132ac
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2021-06-17 13:51:06 -0300

    remove method that dont override anything

--- android/src/main/java/com/reactnativeespidfbleprovisioningrn/EspIdfBleProvisioningRnPackage.java
+++ android/src/main/java/com/reactnativeespidfbleprovisioningrn/EspIdfBleProvisioningRnPackage.java
@@ -21,11 +21,6 @@ public class EspIdfBleProvisioningRnPackage implements ReactPackage {
         return modules;
     }
 
-   @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return null;
-    }
-
     @Override
     public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
         return Collections.emptyList();

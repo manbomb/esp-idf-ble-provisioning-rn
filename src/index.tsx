8e3b1ee0e91bde78a3f16bc62a194c0f47464bfc
Sérgio Avilla <sergiolucasavilladasilva@gmail.com>
2021-06-18 15:11:57 -0300

    create type to lib

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import { NativeModules } from 'react-native';
 
 type EspIdfBleProvisioningRnType = {
-  multiply(a: number, b: number): Promise<number>;
+  create(): void;
+  scanBleDevices(prefix: String): Promise<Array<Object>>;
+  setProofOfPossession(proof: String): void;
+  getProofOfPossession(): Promise<String>;
+  connectToBLEDevice(uuid: String): Promise<Object>;
+  scanNetworks(): Promise<Array<Object>>;
+  sendCustomData(customEndPoint: String, customData: String): Promise<Object>;
+  provisionNetwork(ssid: String, password: String): Promise<Object>;
 };
 
 const { EspIdfBleProvisioningRn } = NativeModules;
 
-export default EspIdfBleProvisioningRn;
+export default EspIdfBleProvisioningRn as EspIdfBleProvisioningRnType;

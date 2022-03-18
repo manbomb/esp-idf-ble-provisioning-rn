c5c739b806be29f4f2bb6031de1ffb70171d6c13
Max Thom Stahl <max@88hustles.com>
2022-03-18 14:20:35 -0700

    Fixed broken NonNull import

--- android/src/main/java/com/reactnativeespidfbleprovisioningrn/EspIdfBleProvisioningRnPackage.java
+++ android/src/main/java/com/reactnativeespidfbleprovisioningrn/EspIdfBleProvisioningRnPackage.java
@@ -1,10 +1,8 @@
 package com.reactnativeespidfbleprovisioningrn;
 
-import android.support.annotation.NonNull;
-
+import androidx.annotation.NonNull;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
